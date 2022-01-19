a1ad3332ebe497d541e219b677791a56b80da617
Rayzor Arboleya <rayzor@arboleya.me>
2022-01-19 15:39:38 -0300

    1 part done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -47,3 +47,10 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = Chronometer;
+}
